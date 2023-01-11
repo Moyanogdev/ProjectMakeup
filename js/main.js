ecdc2e5f0d55c5a8a4298301bc50fefc7225d42c
Gonzalo Moyano <gmoyanodvlpr@gmail.com>
2023-01-11 18:13:59 -0300

    EmailJs

--- js/main.js
+++ js/main.js
@@ -1,55 +1,55 @@
 // SCROLL REVEAL EFECTS
 
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 250,
-});
+    const sr = ScrollReveal({
+        origin: 'top',
+        distance: '60px',
+        duration: 2500,
+        delay: 250,
+    });
 
-sr.reveal(`.tituloJanet`, {origin: 'left'})
-sr.reveal(`.imgJanet`, {origin: 'right'})
-sr.reveal(`.mainHome2`, {origin: 'bottom'})
-sr.reveal(`#mainService`, {origin: 'left'})
-sr.reveal(`#mainService2`, {origin: 'bottom'})
-sr.reveal(`#mainGalery`, {origin: 'bottom'})
-sr.reveal(`.socialEfect`, {origin: 'left'})
-sr.reveal(`.artistEfect`, {origin: 'left'})
-sr.reveal(`.imgPresentacion`, {origin: 'left'})
-sr.reveal(`.textoPresentacion`, {origin: 'right'})
-sr.reveal(`.estudiosEfect`, {origin: 'bottom'})
-sr.reveal(`.mapContacto`, {origin: 'bottom'})
-sr.reveal(`#sectionContacto`, {origin: 'bottom'})
+    sr.reveal(`.tituloJanet`, {origin: 'left'})
+    sr.reveal(`.imgJanet`, {origin: 'right'})
+    sr.reveal(`.mainHome2`, {origin: 'bottom'})
+    sr.reveal(`#mainService`, {origin: 'left'})
+    sr.reveal(`#mainService2`, {origin: 'bottom'})
+    sr.reveal(`#mainGalery`, {origin: 'bottom'})
+    sr.reveal(`.socialEfect`, {origin: 'left'})
+    sr.reveal(`.artistEfect`, {origin: 'left'})
+    sr.reveal(`.imgPresentacion`, {origin: 'left'})
+    sr.reveal(`.textoPresentacion`, {origin: 'right'})
+    sr.reveal(`.estudiosEfect`, {origin: 'bottom'})
+    sr.reveal(`.mapContacto`, {origin: 'bottom'})
+    sr.reveal(`#sectionContacto`, {origin: 'bottom'})
 
 
 
 // CONTACT FORM
 
-/*
-const btn = document.getElementById('button');
-const msjConfirm = document.getElementById("msjConfirm");
 
-document.getElementById('form')
-.addEventListener('submit', function(event) {
-event.preventDefault();
+    const btn = document.getElementById('button');
+    const msjConfirm = document.getElementById("msjConfirm");
 
-btn.value = ' Enviando... ';
+    document.getElementById('form')
+    .addEventListener('submit', function(event) {
+    event.preventDefault();
 
-const serviceID = '';
-const templateID = ''; 
+    btn.value = ' Enviando... ';
 
-emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-        msjConfirm.innerHTML = "Mensaje enviado correctamente"
-        setTimeout(function(){
-            msjConfirm.innerHTML = ""
-        },5000)
-        form.reset()
-    }, (err) => {
-        btn.value = 'Enviar';
-        alert(JSON.stringify(err));
-        });
-});
+    const serviceID = 'default_service';
+    const templateID = 'template_09idt1o'; 
 
-*/
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            msjConfirm.innerHTML = "Mensaje enviado correctamente"
+            setTimeout(function(){
+                msjConfirm.innerHTML = ""
+            },5000)
+            form.reset()
+        }, (err) => {
+            btn.value = 'Enviar';
+            alert(JSON.stringify(err));
+            });
+    });
+
+
 
